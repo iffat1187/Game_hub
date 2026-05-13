049273b173bca39e96561b811c9788e8e0685b69
iffatjahan1187 <iffatjahan1470@gmail.com>
2026-05-13 06:01:48 +0600

    added genre list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+
 
 
 function App() {
@@ -18,7 +20,8 @@ function App() {
       </GridItem>
       <Show above="lg">
       <GridItem area="aside" >
-       aside
+       <GenreList/>
+
       </GridItem>
       </Show>
       <GridItem area="main" >
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,10 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import { Game } from '../hooks/useGames'
-// import getCroppedImageUrl from '../services/image-url'
-// import CriticScore from './CriticScore'
-// import Emoji from './Emoji'
-// import PlatformIconList from './PlatformIconList'
+ import getCroppedImageUrl from '../services/image-url'
+ import CriticScore from './CriticScore'
+ import Emoji from './Emoji'
+ import PlatformIconList from './PlatformIconList'
 
 interface Props {
   game: Game
